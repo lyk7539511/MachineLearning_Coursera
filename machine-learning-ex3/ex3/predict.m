bd85a82ee831273725a7ac17acef81f8900c34d1
lyk7539511 <852486850@qq.com>
2020-02-09 15:23:41 +0800

    Neural Network Prediction Function done

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,7 +21,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
+X = [ones(m,1) X];
+for i=1:m
+    a2 = sigmoid(X(i,:)*Theta1');
+    a2 = [1 a2];
+    a3 = sigmoid(a2*Theta2');
+    [x,p(i)] = max(a3);
+end
 
 
 
